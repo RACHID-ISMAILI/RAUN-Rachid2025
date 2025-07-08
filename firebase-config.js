76eebb5e873ab61b01b517de7b92872a1d4e1b65
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-08 18:14:58 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDRpjiF3EcSA_7TuGkNLp1bxu3iWoY_XnE",
   authDomain: "raun2025-1558d.firebaseapp.com",
   projectId: "raun2025-1558d",
-  storageBucket: "raun2025-1558d.appspot.com",
+  storageBucket: "raun2025-1558d.firebasestorage.app",
   messagingSenderId: "133273741918",
   appId: "1:133273741918:web:a32b098aac154e42a062cf"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
 
-export { db, auth };
+// ✅ Export to be used in other modules
+export { firebaseConfig };
