da1f55000ee5aba63974338faf20c75e110633f7
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-08 13:51:13 +0100

    Add files via upload

--- ajouter-capsule.js
+++ ajouter-capsule.js
@@ -1,3 +1,4 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { firebaseConfig } from "./firebase-config.js";
